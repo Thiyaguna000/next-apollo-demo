dc75381619f109b20496391112746bc166c064ac
Thiyagu <thiyaguna000@gmail.com>
2022-02-11 03:09:39 +0530

    Heroku setup for nextjs

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c204ddbd0f58cf45"; }
+/******/ 		__webpack_require__.h = function() { return "b35bd5572add58a2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

1ae39079c344da1085d76236a5d69cfacd552add
Thiyagu <thiyaguna000@gmail.com>
2022-02-11 03:41:52 +0530

    port setup

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b35bd5572add58a2"; }
+/******/ 		__webpack_require__.h = function() { return "abb5b508848242cc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

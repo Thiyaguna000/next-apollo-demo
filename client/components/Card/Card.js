50992c96f8616c6f8c0298bd1a56c225027b03da
Thiyagu <thiyaguna000@gmail.com>
2022-02-13 22:32:13 +0530

    Style changes

--- client/components/Card/Card.js
+++ client/components/Card/Card.js
@@ -5,7 +5,7 @@ const Card = ({ data }) => (
         <div className={Styles.cardData}>
             <h2 className={Styles.header} title={data.name}>{data.name}</h2>
             <p className={Styles.subHeader}>Location:<span className={Styles.location}>{data.location}</span></p>
-            <p>Website: <a href={data.webPages} target="_blank">{data.webPages}</a></p>
+            <p className={Styles.webPages} title={data.webPages}>Website: <a href={data.webPages} target="_blank">{data.webPages}</a></p>
         </div>     
     )
 
--- client/pages/index.js
+++ client/pages/index.js
@@ -9,7 +9,7 @@ const Page = () => {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@1,300&display=swap" rel="stylesheet" />
         </Head>
-        <div>
+        <div style={{ textAlign: "center"}}>
           <h1>Welcome</h1>
           <br/><br/>
           <Link href="/about"><a>About</a></Link>
